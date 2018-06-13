1b17a823d222688d16a8a29d091c4f155cec4338
unfaiyted <unfaiyted@gmail.com>
2018-06-13 08:40:20 -0500

    update to design issue with z index on project view

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -17,19 +17,15 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping(path = "/roll-dice", method = RequestMethod.GET)
-    public String rollMain() {
-        return "roll-dice";
+
+
+    @RequestMapping(path = "/resume", method = RequestMethod.GET)
+    public String resume() {
+        return "personal/resume";
     }
 
-    @RequestMapping(path = "/roll-dice/{number}", method = RequestMethod.GET)
-    public String roll(@PathVariable String number, Model model) {
 
-        int randomNum = ThreadLocalRandom.current().nextInt(1, 7);
 
-        model.addAttribute("page_submitted",true);
-        model.addAttribute("roll_actual", randomNum);
-        model.addAttribute("roll_guess",number);
-        return "roll-dice";
-    }
+
+
 }
